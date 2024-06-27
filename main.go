c75521b6e4f219ca17322b15479bbb2d9a14b072
branilaa <stefan.branila55@gmail.com>
2024-06-27 12:04:54 +0200

    code refactoring

--- main.go
+++ main.go
@@ -8,6 +8,28 @@ import (
 )
 
 func main() {
+	source := getSourceFile()
+	defer source.Close()
+
+	output := getOutputFile(source.Name())
+	defer output.Close()
+
+	transpile(source, output)
+}
+
+func transpile(source, output *os.File) {
+	scanner := bufio.NewScanner(source)
+
+	// Reads the file line by line
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Writes the line to the output file
+		output.WriteString(line)
+	}
+}
+
+func getSourceFile() *os.File {
 	// Checks if the user provided an input file
 	if len(os.Args) == 1 {
 		log.Fatal("No input file provided")
@@ -26,23 +48,15 @@ func main() {
 		log.Fatal("An error occurred while reading the file:", err)
 	}
 
-	defer source.Close()
+	return source
+}
 
+func getOutputFile(sourceName string) *os.File {
 	// Creates the output file
 	output, err := os.Create(sourceName[:len(sourceName)-3] + ".js")
 	if err != nil {
 		log.Fatal("An error occurred while creating the output file:", err)
 	}
 
-	defer output.Close()
-
-	scanner := bufio.NewScanner(source)
-
-	// Reads the file line by line
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		// Writes the line to the output file
-		output.WriteString(line)
-	}
+	return output
 }
